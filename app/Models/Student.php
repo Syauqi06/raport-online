228d6683cbe967270fdab9d64159de239a389962
Syauqi06 <syauqi0066@gmail.com>
2025-12-08 14:18:18 +0700

    Model Student and Teachers

---
+++ app/Models/Student.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'classroom_id',
+        'nis',
+        'nisn',
+        'address',
+        'birth_date',
+        'birth_place',
+        'phone',
+        'address',
+    ];
+
+    // Relasi ke User
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Relasi ke Classroom
+    public function classroom(): BelongsTo
+    {
+        return $this->belongsTo(Classroom::class);
+    }
+}
